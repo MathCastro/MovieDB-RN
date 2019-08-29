5e2553bd390f71df299780f167d0cc50edb2b4e4
Matheus Feitosa de Castro <mathcastro@MacBook-Air-de-Matheus.local>
2019-08-29 18:19:31 -0300

    initial step for login screen

--- src/view/unauthorized/login-screen/LoginScreen.js
+++ src/view/unauthorized/login-screen/LoginScreen.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
 import Styles from './LoginScreen.styles';
 
-const LoginScreen = () => {
+const LoginScreen = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    console.log(email, password);
+    setEmail(email);
+    setPassword(password);
+  }, [email, password]);
+
   return (
     <View styles={Styles.container}>
-      <Text>Login Screen</Text>
+      <TextInput onChange={event => setPassword(event.target.value)} />
+      <TextInput onChange={event => setEmail(event.target.value)} />
+      <Button
+        title="Button"
+        onPress={() => {
+          props.navigation.navigate('App');
+        }}
+      />
+      <Text>{(email, password)}</Text>
     </View>
   );
 };
